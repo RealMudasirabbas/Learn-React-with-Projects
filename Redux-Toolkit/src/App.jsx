f211e1dcfbf6765beaa726260b8cf89741f8cf21
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-02-12 13:48:13 +0500

    redux-toolkit completed

---
+++ Redux-Toolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className='text-center font-bold my-2'>Learn Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
