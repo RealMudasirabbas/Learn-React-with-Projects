a6636d00ddf7ff054d2d1acf70d384179e6c38d9
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-02-29 15:23:04 +0500

    Almost Completed

--- Blog-website/src/components/index.js
+++ Blog-website/src/components/index.js
@@ -1,4 +1,10 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Container from './container/Container';
+import Button from './Button';
+import Input from './Input';
+import RTE from './RTE';
 
-export  {Header,Footer}
+export { Header, Footer, Logo, LogoutBtn, Container, Button, Input, RTE };
