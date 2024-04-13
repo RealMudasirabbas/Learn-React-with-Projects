03d6ba55b37d1ce936d3deedf34fc102535e6e17
RealMudasirabbas <mudasirabbas5638201@gmail.com>
2024-04-13 11:46:44 +0500

    completed css

--- ecommerce-app/src/components/Navbar.jsx
+++ ecommerce-app/src/components/Navbar.jsx
@@ -3,8 +3,20 @@ import '../css/navbar.scss';
 function Navbar() {
     return (
         <nav>
-            <div className="left">left</div>
-            <div className="right">right</div>
+            <div className="left">
+                <a href="/" className="logo">
+                    <img src="/public/logo.png" alt="MyState" />
+                <span>MudasirState</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                <a href="/">Login</a>
+                <a href="/" className='register'>SignUp</a>
+            </div>
         </nav>
     );
 }
